2054bfbc07bec7cbbf91e19fc07eb8ac13f6929b
Enishc <enishacisevki@gmail.com>
2024-03-12 21:34:28 +0300

    Son hal (hikayesis)

---
+++ inc/resimler.h
@@ -0,0 +1,76 @@
+//
+// Created by Enis on 12.03.2024.
+//
+
+#ifndef COSMIC_VOYAGER_ADJUNCTLECTURER_RESIMLER_H
+#define COSMIC_VOYAGER_ADJUNCTLECTURER_RESIMLER_H
+#include <iostream>
+namespace resimler {
+
+    std::string normal_gemi_resmi =
+            "           |                                                     \n"
+            "          / \\                                                       \n"
+            "         /   \\                                                           \n"
+            "        /_____\\                                                         \n"
+            "        | (_) |                                                            \n"
+            "        |     |                                                            \n"
+            "        | ETU |                                                             \n"
+            "        |  _  |                                                          \n"
+            "        | | | |                                                       \n"
+            "        | | | |                                                      \n"
+            "        | | | |                                                         \n"
+            "       /  | |  \\                                                              \n"
+            "      /   | |   \\                                                              \n"
+            "     /    | |    \\                                                             \n"
+            "    /_____|_|_____\\                                                   \n"
+            "         |(_)|                                                            \n"
+            "        /|||||\\                                                        \n"
+            "       //|||||\\\\  \n";
+
+
+
+
+    std::string hizli_gemi_resmi =
+            "              /\\    |    /\\                                       \n"
+            "             /__\\  / \\  /__\\                                    \n"
+            "             |  | /   \\ |  |                                      \n"
+            "             |  |/_____\\|  |                                      \n"
+            "             |  || (_) ||  |                                       \n"
+            "             |  ||     ||  |                                       \n"
+            "             |  || ETU ||  |                                        \n"
+            "             |  ||_____||  |                                        \n"
+            "             |  /|  |  |\\  |                                        \n"
+            "             | / |  |  | \\ |                                        \n"
+            "             |/  |  |  |  \\|                                        \n"
+            "             /   |  |  |   \\                                        \n"
+            "            /    |  |  |    \\                                      \n"
+            "           /     |  |  |     \\                                      \n"
+            "          /______|__|__|______\\                                     \n"
+            "             |__| |(_)| |__|                                         \n"
+            "              |  /|||||\\  |                                          \n"
+            "              | //|||||\\\\ |                                         \n";
+
+
+    std::string guclu_gemi_resmi =
+            "                       |                              \n"
+            "                      / \\                              \n"
+            "                     /   \\                                \n"
+            "                    /_____\\                                 \n"
+            "                   /  | |  \\                                 \n"
+            "                  /   | |   \\                                  \n"
+            "                 /   /ETU\\   \\                                    \n"
+            "                /___/ | |_\\___\\                                  \n"
+            "                |  /  | |  \\  |                              \n"
+            "                | /   | |   \\ |                               \n"
+            "                |/    | |    \\|                                \n"
+            "                /     | |     \\                                      \n"
+            "               /      | |      \\                                        \n"
+            "              /       | |       \\                                        \n"
+            "             /________|_|________\\                                          \n"
+            "                |_____(_)_____|                                    \n"
+            "                    /|||||\\                                  \n"
+            "                   //|||||\\\\                       \n";
+
+}
+
+#endif //COSMIC_VOYAGER_ADJUNCTLECTURER_RESIMLER_H
